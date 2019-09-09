b5d2cab44aa3149d20cfb26467c42d63c5dc5e36
Upkar Lidder <lidderupk@gmail.com>
2019-09-08 22:04:24 -0700

    adding date to sms

--- javascript/sendsms/sendsms.js
+++ javascript/sendsms/sendsms.js
@@ -4,9 +4,12 @@ function main(params) {
 
   return new Promise(function (resolve, reject) {
     const client = twilio(params.accountSid, params.authToken);
+    const currentDate = new Date();
+    const date = `${currentDate.getDate()}/${(currentDate.getMonth() + 1)}/${currentDate.getFullYear()}`;
+    const time = `${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`;
     client.messages
       .create({
-        body: 'Welcome to the openwhisk class at forwardJS',
+        body: `Welcome to the openwhisk class at forwardJS ${date} @${time}`,
         from: params.twilio_phone,
         to: params.user_phone
       }).then(message => {
